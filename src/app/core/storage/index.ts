6b05ea2bb08c29da73a5c6322090d0f2fa62f82f
Sergey Prontsevich <prontsevich@gmail.com>
2021-11-17 20:58:17 +0300

    feat(store): created root store

---
+++ src/app/core/storage/index.ts
@@ -0,0 +1,5 @@
+export * from './interfaces/storage.interface';
+export * from './storages/local.storage';
+export * from './storages/memory.storage';
+export * from './storages/session.storage';
+export * from './utils/storage.util';
